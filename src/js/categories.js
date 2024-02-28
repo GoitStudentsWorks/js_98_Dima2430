1b03c0cf0664f731ad326281e539661b7fac52e0
za3DopoGo <143600017+za3DopoGo@users.noreply.github.com>
2024-02-28 22:40:27 +0200

    category

---
+++ src/js/categories.js
@@ -0,0 +1,92 @@
+import axios from 'axios';
+
+const bookCardBtn = document.querySelector('.book-card-btn');
+// const chooseButton = document.querySelector('.buttonChoose');
+// const categoriesSelect = document.getElementById('categories');
+let selectedCategory = '';
+
+
+bookCardBtn.addEventListener('click', function() {
+    selectedCategory = bookCardBtn.dataset.category;
+    
+    console.log(selectedCategory);
+    loadBooks(selectedCategory)
+    .then((result) => {
+        renderBooks(result, selectedCategory);
+
+    }).catch((err) => {
+        
+    });
+
+});
+
+async function loadBooks(category) {
+    try{
+        
+        const response = await axios.get(`https://books-backend.p.goit.global/books/category?category=${category}`);
+        return response.data;
+        
+    }
+    catch(error){
+        console.log(error);
+        return;
+    }
+}
+
+function booksMarkup({_id, book_image, title, author}){
+    return `
+    <li class="book-card" data-id="${_id}">
+      <div class="book-thumb">
+        <img class="book-cover" src="${book_image}" alt="${title}"/>
+        </div>
+        <div class="book-descr">
+        <h2 class="book-name">${title}</h2>
+        <h3 class="book-author">${author}</h3>
+        </div>
+      </li>`;
+}
+
+function renderBooks(books, nc){
+    // separatesWordsAddToTitle(nc);
+    const markup = books.map(booksMarkup).join('');
+    const categoriesList = `<ul class="category-books">${markup}</ul>`;
+    const booksCategory = `<h2 class="category-title">${nc}</h2>${categoriesList}`;
+    console.log(markup);
+    const booksContainer = document.getElementById('books-container');
+    booksContainer.innerHTML = booksCategory;
+}
+
+
+// // Додавання назви категорії до заголовку та кольору
+// function separatesWordsAddToTitle(event) {
+    
+//     const categoryBooksTitle = document.querySelector('.');
+//     const currentCategory = event.target.textContent;
+//     const arrrayCurrentCategory = currentCategory.split(' ');
+//     const lastElementBookTitle = arrrayCurrentCategory.pop();
+//     const wordsOfCategoryTitle = arrrayCurrentCategory.join(' ');
+  
+//     categoryBooksTitle.textContent = wordsOfCategoryTitle;
+//     const textEl = document.createElement('span');
+//     textEl.classList.add('last_word_category_title');
+//     textEl.textContent = lastElementBookTitle;
+//     categoryBooksTitle.appendChild(textEl);
+  
+    
+//   }
+
+//   // Рендеринг списку книг
+// function renderBooksList(books, event) {
+//     bestsellersContainer.style.display = 'none';
+//     categoryBooksContainer.style.display = 'flex';
+//     separatesWordsAddToTitle(event);
+//     checksBooks(books);
+  
+//     const markup = books.map(book => generateBookMarkup(book)).join('');
+//     categoryBooks.insertAdjacentHTML('beforeend', markup);
+//   }
+
+//   function onButtonClick(event) {
+//     if (event.target.className !== 'category_button') {
+//       return;
+//     }
