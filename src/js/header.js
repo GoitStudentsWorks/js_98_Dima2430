c9970acc7b65987d0fa62b7971090e904291e97c
AlbinaKost <143324469+AlbinaKost@users.noreply.github.com>
2024-02-28 15:41:50 +0200

    ипеао

--- src/js/header.js
+++ src/js/header.js
@@ -2,14 +2,31 @@ const themeSwitch = document.querySelector('.theme-switch-input');
 const body = document.querySelector('body');
 const menuOpenButton = document.querySelector('.menu-open-btn');
 const menuCloseButton = document.querySelector('.menu-close-btn');
+const mobMenu = document.querySelector('.menu-modal');
+const paragraphs = document.querySelectorAll('p');
+const headings = document.querySelectorAll('h1');
+const h2 = document.querySelectorAll('h2');
+const h3 = document.querySelectorAll('h3');
+const h4 = document.querySelectorAll('h4');
+const h5 = document.querySelectorAll('h5');
+const h6 = document.querySelectorAll('h6');
+const a = document.querySelectorAll('a');
 
 // Функція для зміни теми та збереження її в локальному сховищі
 function toggleTheme() {
     if (themeSwitch.checked === false) {
+       paragraphs.forEach(p => p.classList.remove('light-theme'));
+       paragraphs.forEach(p => p.classList.add('dark-theme'));
+      //  h1.forEach(h => h.classList.remove('light-theme'));
+      //  h1.forEach(h => h.classList.add('dark-theme'));
         body.classList.add('light-theme');
         body.classList.remove('dark-theme');
         localStorage.setItem('theme', 'light'); // Зберігаємо стан теми в локальному сховищі
     } else {
+      paragraphs.forEach(p => p.classList.add('light-theme'));
+      paragraphs.forEach(p => p.classList.remove('dark-theme'));
+      // h1.forEach(h => h.classList.add('light-theme'));
+      // h1.forEach(h => h.classList.remove('dark-theme'));
         body.classList.remove('light-theme');
         body.classList.add('dark-theme');
         localStorage.setItem('theme', 'dark'); // Зберігаємо стан теми в локальному сховищі
@@ -67,18 +84,23 @@ themeSwitch.addEventListener('click', toggleFontColor);
  
 // Відкриття меню при кліку на кнопку відкриття
   menuOpenButton.addEventListener('click', () => {
-  mobMenu.classList.add('is-open');
-  menuOpenButton.classList.remove('is-open')
+  mobMenu.classList.add('menu-is-open');
+  menuOpenButton.classList.remove('is-open');
+  document.body.style.overflow = 'hidden';
+  mobMenu.body.style.overflow = 'hidden';
   menuOpenButton.classList.add('hidden');
   menuCloseButton.classList.remove('hidden');
 });
 
 // Закриття меню при кліку на кнопку закриття
   menuCloseButton.addEventListener('click', () => {
-  mobMenu.classList.remove('is-open');
+  mobMenu.classList.remove('menu-is-open');
   menuOpenButton.classList.remove('hidden');
-  menuCloseButton.classList.add('hidden');
+  document.body.style.overflow = 'auto';
+  mobMenu.body.style.overflow = 'auto';
+   menuCloseButton.classList.add('hidden');
+ });
 
-});
+
 
 
