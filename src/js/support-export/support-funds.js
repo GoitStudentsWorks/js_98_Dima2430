92fefc7623c3518851496abb1c6fe3c5fa12ad26
svout <ivankovalchuk.service@gmail.com>
2024-02-29 13:16:20 +0200

    Summary

---
+++ src/js/support-export/support-funds.js
@@ -0,0 +1,56 @@
+export const funds = [
+	{
+		 title: 'Save the Children',
+		 url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+		 img: './src/img/save.png',
+		 img2x: './src/img/save.png',
+	},
+	{
+		 title: 'Project HOPE',
+		 url: 'https://www.projecthope.org/country/ukraine/',
+		 img: '',
+		 img2x: '',
+	},
+	{
+		 title: 'UNITED24',
+		 url: 'https://u24.gov.ua/uk',
+		 img: '',
+		 img2x: '',
+	},
+	{
+		 title: 'International Medical Corps',
+		 url: 'https://internationalmedicalcorps.org/country/ukraine/',
+		 img: '',
+		 img2x: '',
+	},
+	{
+		 title: 'Medicins Sans Frontieres',
+		 url: 'https://www.msf.org/ukraine',
+		 img: '',
+		 img2x: '',
+	},
+	{
+		 title: 'RAZOM',
+		 url: 'https://www.razomforukraine.org/',
+		 img: '',
+		 img2x: '',
+	},
+	{
+		 title: 'Action against hunger',
+		 url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+		 img: '',
+		 img2x: '',
+	},
+	{
+		 title: 'World vision',
+		 url: 'https://www.wvi.org/emergencies/ukraine',
+		 img: '',
+		 img2x: '',
+	},
+	{
+		 title: 'Serhiy Prytula Charity Foundation',
+		 url: 'https://prytulafoundation.org/en',
+		 img: '',
+		 img2x: '',
+	},
+]
